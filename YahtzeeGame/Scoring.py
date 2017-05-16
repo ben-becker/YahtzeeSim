e6e4eeae59ad40754261a4e4680f3969664852fc
Philip Andrews <philipa.1842@gmail.com>
2017-05-16 15:52:43 -0500

    Updated Scoring method

--- YahtzeeGame/Scoring.py
+++ YahtzeeGame/Scoring.py
@@ -11,21 +11,12 @@ class Scoring:
     def score_options(dice_list):
 
 
-        scores = {'ones':0, 'twos':0, 'threes':0, 'fours':0, 'fives':0, 'sixes':0, 'threekind':0, 'fourkind':0,\
-                  'fullhouse':0, 'smstraight':0, 'lgstraight':0, 'yahtzee':0, 'chance':0}
+        scores = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-        #ones
-        count = 0
-        for count in (0,6)
-            if dice_list[count] == 1:
-                scores['ones'] = scores['ones'] + 1
-            count = count + 1
-        #twos
-        count = 0
-        for count in (0,6)
-            if dice_list[count] == 2:
-                scores['twoes'] = scores['twoes'] + 2
-            count = count + 1
-        #threes
-        count = 0
-        scores[count-1] = dice_list[0] + dice_list[1] + dice_list[2] + dice_list[3] + dice-list[4]
+        # writes sum of ones-sixes
+        for count in (0,6):
+            if dice_list[count] == count + 1:
+                scores[count] = scores[count] + 1
+
+        #writes chance
+        scores[] = dice_list[0] + dice_list[1] + dice_list[2] + dice_list[3] + dice-list[4]
