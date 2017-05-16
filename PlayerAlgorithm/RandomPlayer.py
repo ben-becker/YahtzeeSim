bc8e61a51d513e23667827a0490a313259672ac0
Ben Becker <benbecker99@gmail.com>
2017-05-16 14:05:22 -0500

    Completed RandomPlayer.py

--- PlayerAlgorithm/RandomPlayer.py
+++ PlayerAlgorithm/RandomPlayer.py
@@ -1,8 +1,18 @@
-# Reserved by Ben
+# Unreserved
 # RandomPlayer makes random decisions
 
+from random import randint
 from PlayerAlgorithm import Player
 
 
 class RandomPlayer(Player):
-    pass
+
+    def get_decision(self, dice_list, scorecard, num_hands, num_rolls):
+        """Method to return random decisions"""
+        decision_list = []
+        for i in range(1, 6):
+            if randint(0, 1) == 0:
+                decision_list.append(True)
+            else:
+                decision_list.append(False)
+        return decision_list
