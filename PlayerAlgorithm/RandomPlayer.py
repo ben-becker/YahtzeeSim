7c2fc81b818ba4998206d3e54246d04e8910ef43
Ben Becker <benbecker99@gmail.com>
2017-05-16 14:06:23 -0500

    Fixed bug in RandomPlayer.py

--- PlayerAlgorithm/RandomPlayer.py
+++ PlayerAlgorithm/RandomPlayer.py
@@ -10,7 +10,7 @@ class RandomPlayer(Player):
     def get_decision(self, dice_list, scorecard, num_hands, num_rolls):
         """Method to return random decisions"""
         decision_list = []
-        for i in range(1, 6):
+        for i in range(0, 6):
             if randint(0, 1) == 0:
                 decision_list.append(True)
             else:
