5fe55028c2fbfbb40dd2af80cdef2dd72ebcc38e
Ben Becker <benbecker99@gmail.com>
2017-05-16 15:58:24 -0500

    Updated Yahtzee.py

--- YahtzeeGame/Scoring.py
+++ YahtzeeGame/Scoring.py
@@ -2,21 +2,21 @@
 # has a method that returns different scores with different choices of scoring options
 
 
-
 class Scoring:
     """Provides methods to return possible scoring options"""
 
-
-
     def score_options(dice_list):
-
-
         scores = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-        # writes sum of ones-sixes
-        for count in (0,6):
-            if dice_list[count] == count + 1:
-                scores[count] = scores[count] + 1
+        # writes scoring for ones-sixes
+        for number in range(1, 7):
+            for dice_index in range(0, 6):
+                if dice_list[dice_index] == number:
+                    scores[number-1] += number
 
-        #writes chance
-        scores[12] = dice_list[0] + dice_list[1] + dice_list[2] + dice_list[3] + dice-list[4]
+        # need scoring for small/large straight
+
+        # need scoring for three/four/five kind
+
+        # writes scoring for chance
+        scores[12] = sum(dice_list for dice_list in dice_list)
